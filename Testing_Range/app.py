5ca2032835cb1845f3a358d49dd7a1bd03869d5d
leung372 <chingheikelvin.leung@mail.utoronto.ca>
2025-06-08 15:40:54 -0400

    new app.py

--- Testing_Range/app.py
+++ Testing_Range/app.py
@@ -1,51 +1,63 @@
+import requests
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
+import base64
+import io
+import re
+from werkzeug.utils import secure_filename
 
-
-"""
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
-
-
-
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
-with app.app_context():
-    db.create_all()
+file = None
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    image_data = None
     if request.method == 'POST':
-        """
-        return 'Hello'
-        """
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        file = request.files['image']
+        if file:
+            image_read_stream = file.read()
+            image_encoded = base64.b64encode(image_read_stream).decode('utf-8')
+            image_type = file.content_type  # e.g., image/png
+            image_data = f'data:{image_type};base64,{image_encoded}'
+    return render_template('uploadImage.html', image_data=image_data)
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            print(new_task.id)  # Should show a valid ID if committed
-            return redirect('/')
-        except:
-            return "There was an issue adding your task"
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+
+@app.route('/submit', methods=['POST'])
+def send_image():
+    data = request.get_json()
+    if not data or 'image_base64' not in data:
+        return 'No image data provided', 400
+
+    image_base64 = data['image_base64']
+
+    # image_base64 is like "data:image/png;base64,iVBORw0KGgoAAAANS..."
+    # You want to strip the prefix before decoding:
+    header, encoded = image_base64.split(',', 1)
+
+    file_type = header.split(';')[0].split(':')[1]
+
+    image_type = file_type.split('/', 1)[1]
+
+    image_name = "receipt." + image_type
+
+    try:
+        image_bytes = base64.b64decode(encoded)
+    except Exception as e:
+        return f'Invalid base64 data: {str(e)}', 400
+    image_file = io.BytesIO(image_bytes)
+    files = {
+        'image': (image_name, image_file, file_type)  # name, content, MIME type
+    }
+
+    response = requests.post('http://127.0.0.1:5001/upload', files=files)
+    return 'Image received successfully', 200
+    # Send to external Flask server
+
 
 
 
 if __name__ == "__main__":
     import os
     app.run(debug=True, use_reloader=False)
-"""
